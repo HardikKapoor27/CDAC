1592adcba7c58e25132b877a0819ae59dccdd88f
HARDIK KAPOOR <161691002+HardikKapoor27@users.noreply.github.com>
2026-03-02 20:15:05 +0530

    Refactor SwitchCalculator class and improve formatting

--- SwitchCalculator.java
+++ SwitchCalculator.java
@@ -1,8 +1,9 @@
-package coreJava;
+package cdac;
 
 import java.util.Scanner;
 
 public class SwitchCalculator {
+	
 
     public static void main(String[] args) {
 
@@ -11,52 +12,56 @@ public class SwitchCalculator {
 
         while (true) {
 
-            System.out.println("\nMenu : \n");
+            System.out.println("\nMenu :");
             System.out.println("1: Addition");
             System.out.println("2: Subtract");
             System.out.println("3: Multiply");
             System.out.println("4: Divide");
-            System.out.println("5: Exit\n");
+            System.out.println("5: Exit");
 
-            System.out.print("Enter your choice: \n");
+            System.out.print("\nEnter your choice: ");
             choice = sc.nextInt();
 
             if (choice == 5) {
-                System.out.println("!! Exiting Calculator...");
+                System.out.println("\n!! Exiting Calculator...");
                 break;
             }
 
-            System.out.print("Enter First Number: ");
+            System.out.print("\nEnter First Number: ");
             int num1 = sc.nextInt();
 
-            System.out.print("\nEnter Second Number: \n");
+            System.out.print("Enter Second Number: ");
             int num2 = sc.nextInt();
+
+            System.out.println();
 
             switch (choice) {
 
                 case 1:
-                    System.out.println("Result: \n" + Choices.Add(num1, num2));
+                    System.out.println("Result: " + Choices.Add(num1, num2));
                     break;
 
                 case 2:
-                    System.out.println("Result: \n" + Choices.Sub(num1, num2));
+                    System.out.println("Result: " + Choices.Sub(num1, num2));
                     break;
 
                 case 3:
-                    System.out.println("Result: \n" + Choices.Mul(num1, num2));
+                    System.out.println("Result: " + Choices.Mul(num1, num2));
                     break;
 
                 case 4:
                     if (num2 == 0) {
                         System.out.println("Cannot divide by zero !!");
                     } else {
-                        System.out.println("\nResult: " + Choices.Div(num1, num2));
+                        System.out.println("Result: " + Choices.Div(num1, num2));
                     }
                     break;
 
                 default:
                     System.out.println("Invalid Choice !!");
             }
+
+            System.out.println();
         }
 
         sc.close();
